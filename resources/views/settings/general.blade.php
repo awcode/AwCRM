7ecc129075508cef6b7a53639ee84faf199275ee
mnwalker <mark@awcode.com>
2015-05-12 15:43:53 +0700

    Fix config issue

--- resources/views/settings/general.blade.php
+++ resources/views/settings/general.blade.php
@@ -8,11 +8,12 @@
 			<li>{!! $error !!}</li>
 		@endforeach
 	</ul>
-
+	
+	@if(isset($config['primary_currency'])
 	{!! Form::label('primary_currency', 'Currency ID')  !!}
 	{!! Form::text('primary_currency', $config['primary_currency'], array('class'=>' form-control')) !!}
 	{!! Form::hidden('config_fields[]','primary_currency') !!}
-				
+	@endif			
 	
 
 	{!! Form::submit('Save', array('class'=>'btn btn-large btn-primary btn-block'))!!}
