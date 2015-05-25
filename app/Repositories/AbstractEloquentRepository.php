cbf9d47ee411bcf118761a2dc9e15f66c81334eb
mnwalker <mark@awcode.com>
2015-05-25 15:15:35 +0700

    Fix abstract addResultsRow

--- app/Repositories/AbstractEloquentRepository.php
+++ app/Repositories/AbstractEloquentRepository.php
@@ -113,7 +113,7 @@ abstract class AbstractEloquentRepository {
 		$this->Where = NULL;
 		if(count($result)){
         	foreach($result as $k=>$v){
-        		$result = $this->addToResultRow($result);
+        		$result = $this->addToResultRow($v);
         	}
         }
 		return $result;
