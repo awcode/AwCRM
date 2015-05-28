8da6ab1884f0f0c6b6e4475ad758650f00ef145c
mnwalker <mark@awcode.com>
2015-05-28 16:11:17 +0700

    remove csrf filter by default, add back into controllers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel {
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
-		'AwCore\Http\Middleware\VerifyCsrfToken',
+		//'AwCore\Http\Middleware\VerifyCsrfToken',
 	];
 
 	/**
