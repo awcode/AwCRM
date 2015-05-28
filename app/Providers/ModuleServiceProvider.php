2d87001b22c01927ae0c3106ca9eff70db0cb869
mnwalker <mark@awcode.com>
2015-05-28 11:18:48 +0700

    Default provider class for modules

--- app/Providers/ModuleServiceProvider.php
+++ app/Providers/ModuleServiceProvider.php
@@ -1,14 +1,7 @@
 <?php namespace AwCore\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use View;
-use HTML;
 
-use AwCore\Modules\Transportersio\Repositories\TransportLegs\TransportLegsInterface as TransportLegsInterface ;
-use AwCore\Modules\Transportersio\Repositories\Cargo\CargoInterface as CargoInterface ;
-use AwCore\Modules\Transportersio\Repositories\CargoTypes\CargoTypesInterface as CargoTypesInterface ;
-use AwCore\Modules\Transportersio\Repositories\CargoGroups\CargoGroupsInterface as CargoGroupsInterface ;
-use AwCore\Modules\Transportersio\Repositories\VehicleTypes\VehicleTypesInterface as VehicleTypesInterface ;
 
 class Transportersio extends ServiceProvider{
 
