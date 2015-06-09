bdcffd4afc4e92a1992907bbb2cea284f9edb07f
mnwalker <mark@awcode.com>
2015-06-09 17:45:21 +0700

    update modules

--- app/Modules/Orders/Repositories/OrderRows/OrderRowsInterface.php
+++
@@ -1,7 +0,0 @@
-<?php 
-namespace AwCore\Modules\Orders\Repositories\OrderRows;
-
-interface OrderRowsInterface {
-	 public function all();
-
-}
--- app/Modules/Orders/Repositories/Orders/OrdersInterface.php
+++
@@ -1,7 +0,0 @@
-<?php 
-namespace AwCore\Modules\Orders\Repositories\Orders;
-
-interface OrdersInterface {
-	 public function all();
-
-}
